6a612561ff61502053910752c10e1edcff79e4cd
triangle0105 <543443317@qq.com>
2017-02-24 17:25:25 +0800

    add

---
+++ Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestRedis;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RedisHelper redis = new RedisHelper();
+            var list1 = new List<Demo>
+            {
+                new Demo
+                {
+                    Id = 1,
+                    Name = "a"
+                },
+                new Demo
+                {
+                    Id = 2,
+                    Name = "b"
+                },
+            };
+            var task=redis.ListRightPushAsync<Demo>("folertest1", "keytest", list1);
+            //redis.ListRightPush<Demo>("folertest", "keytest", list1[1]);
+            //redis.ListLeftPush<Demo>("folertest", "keytest", list1[1]);
+            //var task=redis.ListRangeAsync<Demo>("folertest", "keytest");
+            var list = task.Result;
+        }
+
+        public class Demo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
